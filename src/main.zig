d50b3416d31943f0b1a00f153e6462638f675cad
Sam Whiteley <whiteleys2@macewan.ca>
2024-10-08 15:56:45 -0600

    Can can access win32 api

--- src/main.zig
+++ src/main.zig
@@ -1,3 +1,10 @@
 const std = @import("std");
 
-pub fn main() !void {}
+const win32 = @import("zigwin32");
+const windows = win32.ui.windows_and_messaging;
+
+const print = std.debug.print;
+pub fn main() !void {
+    const windowClass = windows.WNDCLASS{};
+    print("{any}", .{windowClass});
+}
