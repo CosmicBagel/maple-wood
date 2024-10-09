bec2a77a955c795523a3fd02f3a5e189c63ef47a
CosmicBagel <>
2024-10-09 07:43:29 -0600

    Started stub for wayland init

--- src/main.zig
+++ src/main.zig
@@ -5,6 +5,19 @@ const windows = win32.ui.windows_and_messaging;
 
 const print = std.debug.print;
 pub fn main() !void {
-    const windowClass = windows.WNDCLASS{};
-    print("{any}", .{windowClass});
+    initWayland();
+}
+
+fn initWindows() void {
+    // const windowClass = windows.WNDCLASS{};
+    // print("{any}", .{windowClass});
+}
+
+fn initWayland() void {
+    // https://zig.news/leroycep/wayland-from-the-wire-part-1-12a1
+    // https://zig.news/leroycep/wayland-from-the-wire-part-2-1gb7
+    // get display path
+    // send some packets to it to setup shared memory space
+    // create window
+    // create framebuffer in shared memory space
 }
