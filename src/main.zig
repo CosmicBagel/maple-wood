42c2c48c8900745357ed2824709ea4388690b396
CosmicBagel <>
2024-10-10 21:21:28 -0600

    At least its compiling now

--- src/main.zig
+++ src/main.zig
@@ -32,11 +32,38 @@ fn initWindows() void {
         .hIcon = win32.LoadIconW(null, win32.IDI_APPLICATION),
         .hCursor = win32.LoadCursorW(null, win32.IDC_ARROW),
         .hbrBackground = null,
-        .lpszMenuName = win32.L("yeet"),
-        .lpszClassName = null,
+        .lpszClassName = win32.L("yeet"),
+        .lpszMenuName = null,
     };
-    // windows.CreateWindowEx(dwExStyle: WINDOW_EX_STYLE, lpClassName: ?[*:0]align(1)const u8, lpWindowName: ?[*:0]const u8, dwStyle: WINDOW_STYLE, X: i32, Y: i32, nWidth: i32, nHeight: i32, hWndParent: ?HWND, hMenu: ?HMENU, hInstance: ?HINSTANCE, lpParam: ?*anyopaque)
-    print("{any}", .{windowClass});
+
+    _ = win32.RegisterClass(&windowClass);
+
+    const hwnd = win32.CreateWindowEx(
+        win32.WINDOW_EX_STYLE{},
+        win32.L("yeet"),
+        win32.L("yeet"),
+        win32.WS_OVERLAPPEDWINDOW,
+
+        // size and pos
+        win32.CW_USEDEFAULT,
+        win32.CW_USEDEFAULT,
+        win32.CW_USEDEFAULT,
+        win32.CW_USEDEFAULT,
+
+        // more bs
+        null,
+        null,
+        hInstance,
+        null,
+    );
+
+    if (hwnd == null) {
+        @panic("hwnd is null, fuck you");
+    }
+
+    _ = win32.ShowWindow(hwnd, win32.SHOW_WINDOW_CMD{ .SHOWNORMAL = 0 });
+
+    print("\n\n{any}\n\n", .{windowClass});
 }
 
 fn windowEventHandler(_: win32.HWND, _: u32, _: win32.WPARAM, _: win32.LPARAM) callconv(WINAPI) win32.LRESULT {
